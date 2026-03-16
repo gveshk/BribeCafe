5df2b931814dce8ca1fcb0f0d80f6b530a75bb87
Krishna <krishna@example.com>
2026-03-16 15:00:29 +0000

    fix: Remove terser minification

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()]
 })
